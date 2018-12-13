c5bafc6dff1c5b45d4d99c4c98b9417bbd6894d0
Tyrell Frame <tlframe2@gmail.com>
2018-12-13 02:52:42 -0500

    username shows up after logging in

--- TimeSheet/Controllers/HomeController.cs
+++ TimeSheet/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TimeSheet.Models;
 
@@ -10,9 +11,23 @@ namespace TimeSheet.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<User> _userManager;
+
+        public HomeController(UserManager<User> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await GetCurrentUserAsync();
+
+            var model = new User
+            {
+                FirstName = currentUser.FirstName
+            };
+
+            return View(model);
         }
 
         public IActionResult About()
@@ -39,5 +54,7 @@ namespace TimeSheet.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        private Task<User> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
     }
 }
