00b979d2fac9c3001c27f7f6925fe979e3a73cb2
brianbelcher <43049467+brianbelcher@users.noreply.github.com>
2018-12-12 23:59:47 -0500

    Default os date-time + others

--- TimeSheet/wwwroot/js/site.js
+++ TimeSheet/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+/*
+function myFunction() {
+    var x = document.getElementById("myLocalDate");
+    var defaultVal = x.defaultValue;
+    var currentVal = x.value;
+
+    if (defaultVal == currentVal) {
+        document.getElementById("ClockIn").innerHTML = "Default value and current value is the same: "
+            + x.defaultValue + " and " + x.value
+            + "<br>Change the value of the datetime field to see the difference!";
+    } else {
+        document.getElementById("ClockIn").innerHTML = "The default value was: " + defaultVal
+            + "<br>The new, current value is: " + currentVal;
+    }
+}
+
+document.getElementById('ClockIn').value = new Date().toDateInputValue();
+*/
